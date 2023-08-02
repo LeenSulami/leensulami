121be0a55f23493789ca319da62708f0a2699c0b
LeenAlsulami <90353695+LeenAlsulami@users.noreply.github.com>
2023-08-02 04:36:57 +0300

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+// $(document).ready(function () {
+//   $(".navbar-toggler").click(function () {
+//     $(".navbar-collapse").slideToggle();
+//   });
+// });
+// const sidebarToggle = document.getElementById("sidebarToggle");
+// const sidebar = document.getElementById("sidebar");
+
+// sidebarToggle.addEventListener("click", function () {
+//   sidebar.classList.toggle("active");
+//   sidebarToggle.classList.toggle("active");
+// });
